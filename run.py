c4fdb68e4ddb7280273e5c0049ab79452cf67cdf
Wiley Kestner <wiley@aya.yale.edu>
2018-02-28 23:24:53 -0800

    Use brochure_wsgi-0.0.4

--- run.py
+++ run.py
@@ -1,16 +1,12 @@
 import multiprocessing
+import os
 
 import gunicorn.app.base
-import os
 from brochure_wsgi.brochure_wsgi_application import get_brochure_wsgi_application
 from gunicorn.six import iteritems
 
 
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
+class GunicornApplication(gunicorn.app.base.BaseApplication):
     def __init__(self, application, options=None):
         self.options = options or {}
         self.application = application
@@ -32,7 +28,7 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 if __name__ == "__main__":
     option_dictionary = {
         "bind": '%s:%s' % ('0.0.0.0', os.environ.get("PORT", "8000")),
-        "workers": number_of_workers(),
+        "workers": (multiprocessing.cpu_count() * 2) + 1,
     }
-    StandaloneApplication(application=get_brochure_wsgi_application(),
-                          options=option_dictionary).run()
+    GunicornApplication(application=get_brochure_wsgi_application(),
+                        options=option_dictionary).run()
